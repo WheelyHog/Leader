4c1bc2330b0c0e472102b5a6f7ee37a635c6627a
Sergii Gutsuliak <wheelyhog@gmail.com>
2023-01-15 21:27:46 +0100

    add mobile menu

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+const menu = document.querySelector('.mobile-menu-list');
+
+mobileMenuBtn.addEventListener('click', () => {
+  menu.classList.toggle('mobile-menu-active')
+})
