f3c9e39b7fb0f84910f31232e1ab4a6a5e71ea4b
Sergii Gutsuliak <wheelyhog@gmail.com>
2023-01-17 22:29:21 +0100

    add adaptive to banner-section

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const menu = document.querySelector('.mobile-menu-list');
 
 mobileMenuBtn.addEventListener('click', () => {
-  menu.classList.toggle('mobile-menu-active')
+  menu.classList.toggle('mobile-menu-active');
 })
